9ce33b50d3050e9412d69a5629f254386149aa66
Li Xu Cheng <lixucheng@ziggurat.cn>
2018-06-05 14:25:17 +0800

    debug for CORS

--- web/components/home.vue
+++ web/components/home.vue
@@ -1,3 +1,42 @@
 <template>
-    <img src='../images/logo.png'>
+    <div>
+        <!-- <img src="../images/logo.png"> -->
+        <span v-html="captcha"/>
+        <input v-model="captchaText" type="text">
+        <input type="button" value="validate" @click="validate">
+    </div>
 </template>
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            captcha: '',
+            captchaText: ''
+        };
+    },
+    created() {
+        this.getCaptcha();
+    },
+    methods: {
+        getCaptcha() {
+            const url = 'https://api.baas.ziggurat.cn/v1/toolkit/captcha';
+            axios(url, { withCredentials: true }).then(res => {
+                this.captcha = res.data;
+            }).catch(err => {
+                alert(err);
+            });
+        },
+        validate() {
+            const url = 'https://api.baas.ziggurat.cn/v1/toolkit/validate/' +
+                `${this.captchaText}`;
+            axios(url, { withCredentials: true }).then(res => {
+                alert(res.data.success);
+            }).catch(err => {
+                alert(err);
+            });
+        }
+    }
+};
+</script>
